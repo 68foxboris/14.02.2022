fea8a3bc941609a8c490e6bc9cba61fea81e7aed
68foxboris <boris.fox.solovyev@gmail.com>
2022-02-28 12:51:33 +0200

    df

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -364,4 +364,4 @@ SystemInfo["FrontpanelLEDBlinkControl"] = fileAccess("/proc/stb/fp/led_blink")
 SystemInfo["FrontpanelLEDBrightnessControl"] = fileAccess("/proc/stb/fp/led_brightness")
 SystemInfo["FrontpanelLEDColorControl"] = fileAccess("/proc/stb/fp/led_color")
 SystemInfo["FrontpanelLEDFadeControl"] = fileAccess("/proc/stb/fp/led_fade")
-
+SystemInfo["FCC"] = False
